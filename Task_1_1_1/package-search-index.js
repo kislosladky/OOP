707a7e5ed36419b25827f1f926c387757a409328
kislosladky <kislosladky@users.noreply.github.com>
2023-09-11 13:51:46 +0000

    Deploying to gh-pages from @ kislosladky/OOP@2601ac8b1c03987365192ac69c63e73837eece96 🚀

--- Task_1_1_1/package-search-index.js
+++ Task_1_1_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.example"},{"l":"ru.nsu.kislytsin"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.example"}]
--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Heapsort"},{"p":"ru.nsu.kislytsin","l":"Heapsort"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Heapsort"}]
