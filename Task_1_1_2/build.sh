32c4a1b7bed8c3bddee39581f8a64a5bc393557b
Кислицын Илья Георгиевич <126390826+kislosladky@users.noreply.github.com>
2023-09-23 21:56:56 +0700

    Delete Task_1_1_2 directory

--- Task_1_1_2/build.sh
+++
@@ -1,4 +0,0 @@
-javadoc -d ./build/docs src/main/java/ru/nsu/kislitsyn/heapsort/Heapsort.java
-
-javac -d ./build src/main/java/ru/nsu/kislitsyn/heapsort/Heapsort.java
-java -classpath ./build ru/nsu/kislitsyn/heapsort/Heapsort
