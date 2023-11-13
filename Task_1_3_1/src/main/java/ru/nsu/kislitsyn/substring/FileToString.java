e0ebe08defe9d4e4b2d7df0020ebb02d9900ae64
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2023-11-13 09:58:39 +0700

    a

---
+++ Task_1_3_1/src/main/java/ru/nsu/kislitsyn/substring/FileToString.java
@@ -0,0 +1,79 @@
+package ru.nsu.kislitsyn.substring;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+* Class to work with file and construct substring of the file.
+*/
+public class FileToString {
+    private static final int BUFFER_SIZE = 10000;
+    private BufferedReader reader;
+    private int readFromFile;
+    private final char[] buffer;
+    private int readFromBuffer;
+    private final String filename;
+    private boolean inited;
+    private final int lengthOfSubstring;
+
+    /**
+    * Constructor of the class.
+    *
+    * @param filename name of the file.
+    * @param lengthOfSubstring length of the substring we are searching.
+    */
+    public FileToString(String filename, int lengthOfSubstring) {
+        this.filename = filename;
+        this.readFromBuffer = 0;
+        this.readFromFile = 0;
+        this.inited = false;
+        this.buffer = new char[BUFFER_SIZE];
+        this.lengthOfSubstring = lengthOfSubstring;
+    }
+
+    /**
+    * Function that updates the input string by removing the
+    * first character and adding new one in the end.
+    *
+    * @param string string we need to update using file.
+    *
+    * @return true if the string was updated, otherwise false.
+    *
+    * @throws IOException if the file was not found.
+    */
+    public boolean updateString(StringBuilder string) throws IOException {
+        if (!inited) {
+            inited = true;
+            Path path = Paths.get(filename);
+            reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+            readFromFile = reader.read(buffer, 0, BUFFER_SIZE);
+            if (readFromFile == -1) {
+                reader.close();
+                return false;
+            }
+            string.append(buffer, 0, lengthOfSubstring);
+            readFromBuffer += lengthOfSubstring;
+            return true;
+        } else {
+            if (readFromBuffer < readFromFile) {
+                string.deleteCharAt(0);
+                string.append(buffer[readFromBuffer++]);
+                return true;
+            } else {
+                readFromFile = reader.read(buffer, 0, BUFFER_SIZE);
+                if (readFromFile == -1) {
+                    reader.close();
+                    return false;
+                }
+                readFromBuffer = 0;
+                string.deleteCharAt(0);
+                string.append(buffer[readFromBuffer++]);
+                return true;
+            }
+        }
+    }
+}
---
+++ Task_1_3_1/src/main/java/ru/nsu/kislitsyn/substring/SubstringSearch.java
@@ -0,0 +1,90 @@
+package ru.nsu.kislitsyn.substring;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+* Class that searches substring using Rabin Carp algorithm.
+*/
+public class SubstringSearch {
+    private final String substring;
+    private final List<Long> answer;
+    private final long PRIME_NUMBER = 17;
+    private final FileToString fileToString;
+
+    /**
+    * Getter for answer arrayList.
+    *
+    * @return answer arrayList.
+    */
+    public List<Long> getAnswer() {
+        return answer;
+    }
+
+
+    /**
+    * My hash function.
+    *
+    * @param string string we need to hash.
+    *
+    * @return hash.
+    */
+    private long myHash(String string) {
+        long answ = 0;
+        long power = PRIME_NUMBER;
+        for (int i = 0; i < string.length(); i++) {
+            answ += power * string.charAt(i);
+            power *= PRIME_NUMBER;
+        }
+        return answ;
+    }
+
+    /**
+    * Constructor of class.
+    *
+    * @param substring substring we need to search.
+    * @param filename  file where we need to search the substring.
+    */
+    public SubstringSearch(String substring, String filename) {
+        this.substring = substring;
+        this.answer = new ArrayList<>();
+        this.fileToString = new FileToString(filename, substring.length());
+    }
+
+    /**
+    * Function that finds the substring.
+    */
+    public void rabinKarp() {
+        StringBuilder string = new StringBuilder();
+
+        try {
+            fileToString.updateString(string);
+        } catch (IOException e) {
+            System.out.println("Unable to read file");
+            return;
+        }
+        long hashSubStr = myHash(substring);
+        long hashInpStr = myHash(String.valueOf(string));
+        char firstChar;
+        long powerOfPrime = (long) Math.pow(PRIME_NUMBER, substring.length());
+        long i = 0;
+        boolean run = true;
+        do {
+            firstChar = string.charAt(0);
+
+            if (hashInpStr == hashSubStr) {
+                answer.add(i);
+            }
+            try {
+                run = fileToString.updateString(string);
+            } catch (IOException e) {
+                return;
+            }
+            hashInpStr = ((hashInpStr
+                    - (PRIME_NUMBER * firstChar)) / PRIME_NUMBER)
+                    + powerOfPrime * string.charAt(string.length() - 1);
+            i++;
+        } while (run);
+    }
+}
