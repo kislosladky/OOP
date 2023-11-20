d92aa75183611a9a41ddeac01d9428db054d872f
kislosladky <kislosladky@users.noreply.github.com>
2023-11-20 08:48:15 +0000

    Deploying to gh-pages from @ kislosladky/OOP@bd13a3c4550e9ef0cbf485cfdcff28d98d0fa032 🚀

--- Task_1_4_1/package-search-index.js
+++ Task_1_4_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.kislitsyn.studentsBook"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.kislitsyn.StudentsBook"}]
--- Task_1_4_1/type-search-index.js
+++ Task_1_4_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kislitsyn.studentsBook","l":"Mark"},{"p":"ru.nsu.kislitsyn.studentsBook","l":"StudentsBook"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kislitsyn.StudentsBook","l":"Mark"},{"p":"ru.nsu.kislitsyn.StudentsBook","l":"StudentsBook"}]
