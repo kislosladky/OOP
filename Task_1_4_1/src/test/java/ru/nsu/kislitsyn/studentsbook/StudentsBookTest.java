8d7128a8ac686bed303358593765c39a54e5d309
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2023-11-27 22:07:57 +0700

    the end

--- Task_1_4_1/src/test/java/ru/nsu/kislitsyn/studentsbook/StudentsBookTest.java
+++ Task_1_4_1/src/test/java/ru/nsu/kislitsyn/studentsbook/StudentsBookTest.java
@@ -2,8 +2,9 @@ package ru.nsu.kislitsyn.studentsbook;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
