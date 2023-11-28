5e8c1dbce08b0aeb5349565c6bef8dff45ef0170
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2023-11-28 19:38:17 +0700

    add javadocs

--- Task_1_4_1/src/main/java/ru/nsu/kislitsyn/studentsbook/Subjects.java
+++ Task_1_4_1/src/main/java/ru/nsu/kislitsyn/studentsbook/Subjects.java
@@ -5,9 +5,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+* Class to store marks.
+*/
 public class Subjects {
     private final List<Map<String, Mark>> subjects;
     private boolean retake;
+
+    /**
+    * Constructor that makes the exact number of semesters.
+    *
+    * @param numberOfSemesters number of semesters.
+    */
     public Subjects(int numberOfSemesters) {
         this.retake = false;
         this.subjects = new ArrayList<>();
@@ -16,14 +25,31 @@ public class Subjects {
         }
     }
 
+    /**
+    * Getter for retake flag
+    *
+    * @return retake flag
+    */
     public boolean getRetake() {
         return this.retake;
     }
 
+    /**
+     * Getter for the whole table.
+     *
+     * @return subjects list.
+     */
     public List<Map<String, Mark>> getList() {
         return subjects;
     }
 
+    /**
+    * Adds mark.
+    *
+    * @param semester semester where we need to add mark.
+    * @param subject subject with mark we add.
+    * @param mark mark itself.
+    */
     public void add(int semester, String subject, Mark mark) {
         if (this.getSemester(semester).containsKey(subject)) {
             retake = true;
@@ -31,10 +57,25 @@ public class Subjects {
         this.subjects.get(semester - 1).put(subject, mark);
     }
 
+    /**
+    * Getter for particular semester.
+    *
+    * @param semester number of semester.
+    *
+    * @return map of the semester.
+    */
     public Map<String, Mark> getSemester(int semester) {
         return subjects.get(semester - 1);
     }
 
+    /**
+    * Getter for particular mark.
+    *
+    * @param semester number of semester.
+    * @param subject name of subject.
+    *
+    * @return Mark.
+    */
     public Mark getMark(int semester, String subject) {
         if (!this.getSemester(semester).containsKey(subject)) {
             System.out.println("There is no such subject");
