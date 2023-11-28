a025f00f31ba0ed0237e07c1e1c36ec83da4f55a
kislosladky <kislosladky@users.noreply.github.com>
2023-11-28 12:31:48 +0000

    Deploying to gh-pages from @ kislosladky/OOP@a8ce5b97200a3d04b39e370fcfec91f3f00e7442 🚀

--- Task_1_4_1/type-search-index.js
+++ Task_1_4_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kislitsyn.studentsbook","l":"Mark"},{"p":"ru.nsu.kislitsyn.studentsbook","l":"StudentsBook"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kislitsyn.studentsbook","l":"Mark"},{"p":"ru.nsu.kislitsyn.studentsbook","l":"StudentsBook"},{"p":"ru.nsu.kislitsyn.studentsbook","l":"Subjects"}]
