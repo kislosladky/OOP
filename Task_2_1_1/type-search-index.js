05f34f178c36842dc75f98b2e6c314f8e4b00280
kislosladky <kislosladky@users.noreply.github.com>
2024-02-09 06:58:29 +0000

    Deploying to gh-pages from @ kislosladky/OOP@1bdecbb12d1aaa4085ddbeb3ebd06b6fab00b45e 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Prime"},{"p":"<Unnamed>","l":"PrimeCheck"},{"p":"<Unnamed>","l":"Primes"},{"p":"<Unnamed>","l":"PrimesParallel"},{"p":"<Unnamed>","l":"PrimesThread"}]
