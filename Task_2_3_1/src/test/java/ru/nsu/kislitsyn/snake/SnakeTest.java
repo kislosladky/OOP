75bc80ecda90840c48bf538027c8de4afec68c04
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2024-04-05 11:13:01 +0700

    more tests

--- Task_2_3_1/src/test/java/ru/nsu/kislitsyn/snake/SnakeTest.java
+++ Task_2_3_1/src/test/java/ru/nsu/kislitsyn/snake/SnakeTest.java
@@ -1,8 +1,10 @@
 package ru.nsu.kislitsyn.snake;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class SnakeTest {
     @Test
@@ -29,5 +31,26 @@ class SnakeTest {
     void bumpedTest() {
         Snake snake = new Snake(10, 10);
         assertFalse(snake.bumped());
+    }
+
+    @Test
+    void initTest() {
+        Snake snake = new Snake(10, 10);
+        assertEquals(2, snake.getApples().size());
+        assertEquals(new Point(0,0), snake.getBody().peekFirst());
+    }
+
+    @Test
+    void restartTest() {
+        Snake snake = new Snake(10, 10);
+        snake.moveAndEat();
+        snake.setDirection(Snake.Direction.UP);
+        snake.moveAndEat();
+        snake.restart();
+
+        assertEquals(1, snake.getBody().size());
+        assertEquals(new Point(0,0), snake.getBody().peekFirst());
+        assertEquals(Snake.Direction.RIGHT, snake.getDirection());
+        assertEquals(3, snake.getApples().size());
     }
 }
