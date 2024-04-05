dfc92d62c68e26f35b7cf9a836fc20238d66db4d
kislosladky <kislosladky@users.noreply.github.com>
2024-04-05 04:04:38 +0000

    Deploying to gh-pages from @ kislosladky/OOP@239f2c1e13734b62021d4700539b0160b9804113 🚀

---
+++ Task_2_3_1/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"ru.nsu.kislitsyn.snake"}];updateSearchResults();
---
+++ Task_2_3_1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"ru.nsu.kislitsyn.snake","l":"ru.nsu.kislitsyn.snake"}];updateSearchResults();
